8dfcd27a0450e5660ed264202351e9c940e1a2a7
qhsdnr0 <qhsdnr0@gmail.com>
2022-01-15 20:03:20 +0900

    Modify : FileService 수정
    - parameter를 NoticeForm형으로 받던것을 Notice로 받도록 수정
    >> unitTest의 용이성을 위함

--- src/main/java/rsupport/rsupport/service/NoticeService.java
+++ src/main/java/rsupport/rsupport/service/NoticeService.java
@@ -7,12 +7,10 @@ import rsupport.rsupport.controller.NoticeForm;
 import rsupport.rsupport.domain.File;
 import rsupport.rsupport.domain.Notice;
 import rsupport.rsupport.domain.User;
-import rsupport.rsupport.repository.FileRepository;
-import rsupport.rsupport.repository.NoticeQueryRepository;
-import rsupport.rsupport.repository.NoticeRepository;
-import rsupport.rsupport.repository.UserRepository;
+import rsupport.rsupport.repository.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,33 +19,19 @@ public class NoticeService {
 
     private final NoticeRepository noticeRepository;
     private final NoticeQueryRepository noticeQueryRepository;
-    private final UserRepository userRepository;
     private final FileRepository fileRepository;
+    private final FileQueryRepository fileQueryRepository;
 
-    public void createNotice(NoticeForm noticeForm) {
-        Notice notice = new Notice();
-        User user = userRepository.getById(noticeForm.getUserId());
-
-        notice.setTitle(noticeForm.getTitle());
-        notice.setContent(noticeForm.getContent());
-        notice.setStartAt(noticeForm.getStartAt());
-        notice.setEndAt(noticeForm.getEndAt());
-        notice.setCreatedAt(LocalDateTime.now());
-        notice.setUpdatedAt(LocalDateTime.now());
+    public void createNotice(User user, Notice notice) {
         notice.addUser(user);
-
-        for(String fileUrl : noticeForm.getFileUrls()) {
-            File file = new File();
-            file.setFileUrl(fileUrl);
-            notice.addFiles(file);
-            fileRepository.save(file);
-        }
-
         noticeRepository.save(notice);
-
     }
 
     public Notice getNotice(Long id) {
+        return noticeRepository.getById(id);
+    }
+
+    public Notice getNoticeAndUser(Long id) {
         return noticeQueryRepository.findNoticeAndUser(id);
     }
 
@@ -55,12 +39,17 @@ public class NoticeService {
         noticeRepository.deleteById(id);
     }
 
-    public void updateNotice(Long id, NoticeForm noticeForm) {
-        Notice notice = noticeRepository.getById(id);
-        notice.setTitle(noticeForm.getTitle());
-        notice.setContent(noticeForm.getContent());
-        notice.setStartAt(noticeForm.getStartAt());
-        notice.setEndAt(noticeForm.getEndAt());
-        notice.setUpdatedAt(LocalDateTime.now());
+    public void deleteAllFile(Notice notice) {
+        fileRepository.deleteAll(fileQueryRepository.getFiles(notice.getId()));
+    }
+
+    public void addFiles(Notice notice, List<String> fileUrls) {
+        for(String fileUrl : fileUrls) {
+            File file = new File();
+            file.setNotice(notice);
+            file.setFileUrl(fileUrl);
+            notice.getFiles().add(file);
+            fileRepository.save(file);
+        }
     }
 }
